d1eaa4032b85286c38745da915cb64e889187589
deepika-815 <deepzshetty18@gmail.com>
2026-09-02 20:47:02 +0530

    number analysis.java

---
+++ number analysis.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class NumberAnalysisSimple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+        if (num > 0) {
+            System.out.println(num + " is Positive");
+        } else if (num < 0) {
+            System.out.println(num + " is Negative");
+        } else {
+            System.out.println(num + " is Zero");
+        }
+
+        
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is Prime");
+        } else {
+            System.out.println(num + " is Not Prime");
+        }
+
+        sc.close();
+    }
+}
